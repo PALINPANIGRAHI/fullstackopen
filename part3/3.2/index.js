721dcf88fd8e9569364d9c7202f0904285eda5c8
PALINPANIGRAHI <panigrahipalin@gmail.com>
2025-08-29 18:32:15 +0000

    modified

---
+++ part3/3.2/index.js
@@ -0,0 +1,50 @@
+const express=require('express')
+const app=express()
+app.use(express.json())
+
+let persons=[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+//to print in the frontend
+app.get('/info',(request,response)=>{
+    const date=new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`)
+})
+//to create an array in json format
+app.get('/api/persons',(requset,response)=>{
+    response.json(persons)
+})
+//to display all the info in the backend
+app.get('/api/persons',(request,response)=>{
+    const id=request.params.id
+    const person=persons.find(p=>p.id===id)
+    if(person){
+        response.json(persons)
+    }else{
+        response.status(404).end()
+    }
+})
+const PORT=3001
+app.listen(PORT,()=>{
+    console.log(`Server running in port ${PORT}`)
+})
---
+++ part3/3.4/index.js
@@ -0,0 +1,65 @@
+const express=require('express')
+const app=express()
+app.use(express.json())
+
+let persons=[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+//to print in the frontend
+app.get('/info',(request,response)=>{
+    const date=new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`)
+})
+//to create an array in json format
+app.get('/api/persons',(requset,response)=>{
+    response.json(persons)
+})
+//to display all the info in the backend through id also
+app.get('/api/persons/:id',(request,response)=>{
+    const id=request.params.id
+    const person=persons.find(p=>p.id===id)
+    if(person){
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+})
+//delte data
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id.toString()
+  const initialLength = persons.length
+
+  persons = persons.filter(person => person.id !== id)
+
+  if (persons.length < initialLength) {
+    response.status(204).end()
+  } else {
+    response.status(404).send({ error: 'Person not found' })
+  }
+})
+
+
+const PORT=3001
+app.listen(PORT,()=>{
+    console.log(`Server running in port ${PORT}`)
+})
