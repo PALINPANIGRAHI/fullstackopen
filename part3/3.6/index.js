60a2be5f209a1072de768ee58a34ad1a02809a28
PALINPANIGRAHI <panigrahipalin@gmail.com>
2025-08-30 13:34:11 +0000

    modified

---
+++ part3/3.6/index.js
@@ -0,0 +1,108 @@
+const express=require('express')
+const app=express()
+app.use(express.json())
+
+let persons=[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+//to print in the frontend
+app.get('/info',(request,response)=>{
+    const date=new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`)
+})
+//to create an array in json format
+app.get('/api/persons',(requset,response)=>{
+    response.json(persons)
+})
+//to display all the info in the backend through id also
+app.get('/api/persons/:id',(request,response)=>{
+    const id=request.params.id
+    const person=persons.find(p=>p.id===id)
+    if(person){
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+})
+//delte data
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id.toString()
+  const initialLength = persons.length
+
+  persons = persons.filter(person => person.id !== id)
+
+  if (persons.length < initialLength) {
+    response.status(204).end()
+  } else {
+    response.status(404).send({ error: 'Person not found' })
+  }
+})
+//add data
+function getId(max){
+    return Math.floor(Math.random()*max);
+}
+
+app.post('/api/persons',(request,response)=>{
+    const body=request.body
+
+    //error message for name or number is missing and if number already exists in phonebook
+    if(!body.name || !body.number){
+        return response.status(400).json({
+            error: 'name must be unique'
+    })
+
+    // Check if name already exists
+  const nameExists = persons.find(p => p.name === body.name)
+  if (nameExists) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  // Check if number already exists
+  const numberExists = persons.find(p => p.number === body.number)
+  if (numberExists) {
+    return response.status(400).json({
+      error: 'number must be unique'
+    })
+  }
+}
+
+
+
+  const person= {
+    name: body.name,
+    
+    number: body.number,
+    id: getId(100000),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const PORT=3001
+app.listen(PORT,()=>{
+    console.log(`Server running in port ${PORT}`)
+})
